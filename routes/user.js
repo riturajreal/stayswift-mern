48ce41d1087f17e41814753d192eb37cb45c0d4e
Rituraj Jha <riturajreal01@gmail.com>
2024-01-09 11:56:22 +0530

    added automatically login after signup

--- routes/user.js
+++ routes/user.js
@@ -1,59 +1,69 @@
-const express = require('express');
-const router = express.Router({mergeParams:true});
+const express = require("express");
+const router = express.Router({ mergeParams: true });
 const User = require("../models/user");
-const wrapAsync = require('../utils/wrapAsync');
-const passport = require('passport');
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
 
 // 1. Signup user GET
-router.get('/signup', (req,res)=> {
-    res.render("user/signup.ejs");
+router.get("/signup", (req, res) => {
+  res.render("user/signup.ejs");
 });
 // 2. Signup User POST
-router.post('/signup', wrapAsync(async(req,res)=> {
+router.post(
+  "/signup",
+  wrapAsync(async (req, res) => {
+    try {
+      let { username, email, password } = req.body;
 
-    try{
-        let {username, email, password} = req.body;
+      const newUser = new User({ email, username });
+      const registeredUser = await User.register(newUser, password);
+      // console.log(registeredUser);
 
-        const newUser = new User({email, username});
-        const registeredUser = await User.register(newUser, password);
-        // console.log(registeredUser);
+      // if user is successfully sign up then automatically login user
+      req.login(registeredUser, (err) => {
+        if (err) {
+          return next(err);
+        }
 
         req.flash("success", "Welcome to Wanderlust");
 
-        res.redirect('/listings');
+        res.redirect("/listings");
+      });
+    } catch (e) {
+      req.flash("error", e.message);
+      res.redirect("/signup");
     }
+  })
+);
 
-    catch(e) {
-        req.flash("error", e.message);
-        res.redirect('/signup');
-    }
-}));
-
-// 3. Login user GET 
-router.get("/login",(req,res)=> {
-    res.render("user/login.ejs");
-} );
-
-// 4. Login user POST
-router.post("/login", passport.authenticate("local", { 
-    failureRedirect: '/login', 
-    failureFlash: true // Remove the trailing comma
-}), async (req, res) => {
-    req.flash("success","Welcome back to Wanderlust");
-    res.redirect('listings');
+// 3. Login user GET
+router.get("/login", (req, res) => {
+  res.render("user/login.ejs");
 });
 
+// 4. Login user POST
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    failureRedirect: "/login",
+    failureFlash: true, // Remove the trailing comma
+  }),
+  async (req, res) => {
+    req.flash("success", "Welcome back to Wanderlust");
+    res.redirect("listings");
+  }
+);
 
 // 5. Logout --> req.logout();
-router.get('/logout', (req,res, next) => {
-    req.logout((err) => {
-        if(err) {
-            return next(err);
-        }
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
 
-        req.flash("success", "you are logged out");
-        res.redirect('/listings');
-    })
+    req.flash("success", "you are logged out");
+    res.redirect("/listings");
+  });
 });
 
 module.exports = router;
